bb0506e1f189a23f14c4ba75594aad7e0bb3d152
jorgemonraz <jmonraz@student.fullsail.edu>
2023-06-29 15:40:07 -0700

    added username and password login  validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import InputField from "../InputField";
 import FormButton from "../FormButton";
 import './LoginForm.css';
@@ -6,35 +6,26 @@ import './LoginForm.css';
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [isUsernameEmpty, setIsUsernameEmpty] = useState(false);
-    const [isPasswordEmpty, setIsPasswordEmpty] = useState(false);
+    const [loginMessage, setLoginMessage] = useState('');
+    const formRef = useRef(null);
 
     const handleUsernameChange = event => {
         setUsername(event.target.value);
-        setIsUsernameEmpty(false);
     };
 
     const handlePasswordChange = event => {
         setPassword(event.target.value);
-        setIsPasswordEmpty(false);
     };
 
     const handleSubmit = event => {
-        event.preventDefault();
 
         console.log('username: ', username);
         console.log('password: ', password);
 
-        // validate the inputs
-        if (!username) {
-            setIsUsernameEmpty(true);
-        }
-        if (!password) {
-            setIsPasswordEmpty(true);
-        }
-
         // submit the form if inputs are valid
         if (username && password) {
+
+            event.preventDefault();
             console.log('submit form');
 
             // make API request
@@ -51,25 +42,34 @@ const LoginForm = () => {
                 .then(response => response.json())
                 .then(data => {
                     console.log('API response:', data);
+                    if (data.message == 'User logged in failed') {
+                        setLoginMessage('Invalid username or password entered.');
+                    } else {
+                        setLoginMessage('');
+                        formRef.current.submit();
+                    }
+                    // reset form
+                    setUsername('');
+                    setPassword('');
+                    setIsUsernameEmpty(false);
+                    setIsPasswordEmpty(false);
                 })
                 .catch(error => {
                     console.log('API error:', error);
+                    setLoginMessage('An error occurred. Please try again later.');
                 })
-            // reset form
-            setUsername('');
-            setPassword('');
-            setIsUsernameEmpty(false);
-            setIsPasswordEmpty(false);
+
         }
 
 
     }
     return (
-        <form onSubmit={handleSubmit} className="login-form">
+        <form onSubmit={handleSubmit} className="login-form" ref={formRef}>
             <InputField id="username" type="username" placeholder="Username" value={username} onChange={handleUsernameChange} required />
             {/* {isUsernameEmpty && <span className="error-message">Username is required</span>} */}
             <InputField id="password" type="password" placeholder="Password" value={password} onChange={handlePasswordChange} required />
             {/* {isPasswordEmpty && <span className="error-message">Password is required</span>} */}
+            {loginMessage && <span className="error-message">{loginMessage}</span>}
             <FormButton type="submit" handleSubmit={handleSubmit}>Login</FormButton>
             <FormButton type="submit" handleSubmit={handleSubmit}>Change Password</FormButton>
         </form>
