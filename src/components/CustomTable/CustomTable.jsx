305cbf1b5746e8adbf636b9b1020735dd6502982
jorgemonraz <jmonraz@student.fullsail.edu>
2023-07-01 23:39:43 -0700

    added table component which is going to display information from the database as table in the UI

---
+++ src/components/CustomTable/CustomTable.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./CustomTable.css";
+
+const CustomTable = ({ data, columns }) => {
+    return (
+        <div className="custom-table-container">
+            <table className="custom-table">
+                <thead>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index}>{column}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((row, index) => (
+                        <tr key={index}>
+                            {row.map((cell, cellIndex) => (
+                                <td key={cellIndex}>{cell}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default CustomTable;
--- src/components/PatientsScreen/PatientsScreen.jsx
+++ src/components/PatientsScreen/PatientsScreen.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./PatientsScreen.css";
 import ButtonsRow from "../ButtonsRow";
+import CustomTable from "../CustomTable";
 import addIcon from "../../assets/images/icons/add_icon_green.png";
 import editIcon from "../../assets/images/icons/edit_icon_orange.png";
 import blockIcon from "../../assets/images/icons/block_icon_red.png";
 import searchIcon from "../../assets/images/icons/search_icon_black.png";
 import excelIcon from "../../assets/images/icons/excel_icon_green.png";
-import pdfIcon from "../../assets/images/icons/pdf_icon_red.png";
 import adobeIcon from "../../assets/images/icons/adobe_icon_red.png";
 
 const PatientsScreen = () => {
@@ -19,9 +19,19 @@ const PatientsScreen = () => {
         { 'icon': excelIcon, 'label': 'excel' },
         { 'icon': adobeIcon, 'label': 'adobe' },
     ];
+    const columns = [
+        'Name', 'Age', 'Height', 'Weight', 'Blood Group', 'Registration Date', 'Last Visit'
+    ];
+    const data = [
+        ['Jorge', 28, 511, 190, 'A+', '01/01/2023', '05/01/2023'],
+        ['Aide', 26, 507, 160, 'B-', '01/01/2023', '06/01/2023'],
+        ['Insi qoaksdj', 30, 607, 123020, 'BSJDHKS', '012/123/39943', '01203/1222/12321'],
+        ['Insuspdsuaaaaaaaaa', 30, 40, 10, 'asdasdsadjlasjdlkasjdljsalda', 'asdhjashdjlashdjsahjdhaskdsa', 'ajsldhajslhdjalshdashdjhsajkdhsahdjahdkhsjdhjkahdks']
+    ];
     return (
         <>
             <ButtonsRow buttons={buttons} width={20} />
+            <CustomTable data={data} columns={columns} />
         </>
     )
 }
